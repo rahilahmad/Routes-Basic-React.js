e51a195a478794f5d77667ddda8862f113fef1bb
Raheel Ahmed <p136025@nu.edu.pk>
2018-10-02 11:39:49 +0500

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from 'react-router-dom'; 
+
+const Navigation = () => {
+  return (
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <br />
+      <NavLink to="/about">About</NavLink>
+      <br />
+      
+      <NavLink to="/contact">Contact</NavLink>
+    </div>
+  );
+};
+
+export  {Navigation};
